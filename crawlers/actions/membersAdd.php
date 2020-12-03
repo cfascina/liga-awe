71b044f6bbe87a69cb69e2450def0252fae1fcc3
Caio <cfascina@gmail.com>
2020-12-03 20:42:19 -0300

    Classification table sprt icons

--- crawlers/actions/membersAdd.php
+++ crawlers/actions/membersAdd.php
@@ -2,7 +2,7 @@
 
 require_once '../funcs.php';
 
-$members = file('../members-test.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$members = file('../members.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 foreach($members as $memberId) {
     $memberData = getMemberData($memberId);
