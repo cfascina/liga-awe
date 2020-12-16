645b2004c857e822db5c098dbd677d5753e28941
Caio <cfascina@gmail.com>
2020-12-16 14:23:24 -0300

    Members datatable layout

--- members.php
+++ members.php
@@ -6,38 +6,62 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Liga AWE</title>
 	<link rel="stylesheet" href="./assets/css/styles.css">
+	<link rel="stylesheet" href="./assets/css/datatables.css">
 </head>
 
 <body>
 	<?php require_once('partials/header.html'); ?>
 
 	<div class="content members">
-		<div class="list"></div>
+		<table class="members"></table>
 	</div>
 
 	<?php require_once('partials/footer.html'); ?>
 
 	<script src="./assets/js/libs/jquery.min.js"></script>
+	<script src="./assets/js/libs/datatables.min.js"></script>
 	<script src="./assets/js/services/members.js"></script>
 	<script>
-		function setMembersTable(arrMembers) {
-			arrMembers.forEach(member => {
-				let proStamp = member.pro == 1 ? '<img src="./assets/images/pro.svg" class="pro" />' : '';
+        function handleMembersData(data) {
+            let arrTable = [];
 
-				$('.list').append(
-					'<a href="memberDetails.php?memberId=' + member.id_cartola + '">' +
-						'<img src="' + member.shield + '" class="shield" />' +
-						'<span class="team">' + member.team + '</span>' +
-						'<span class="name">(' + member.name + ')</span>' +
-						proStamp +
-					'</a>'
-				);
-			});
-		}
+            data.forEach(member => {
+                let isPro = member.pro == 1 ? 'Sim' : 'Não';
+
+                arrTable.push({
+                    id_member: '<span class="id-member">' + member.id_cartola + '</span>', 
+                    team: 
+                        '<div class="shield-wrap">' + 
+                            '<img src="' + member.shield + '" />' + 
+                            '<span>' + member.team + '</span>' +
+                        '</div>',
+                    name: '<div class="team">' + member.name + '</div>', 
+                    first_year: member.first_year, 
+                    pro: isPro
+                });
+            });
+
+            setClassificationTable(arrTable);
+        }
+
+        function setClassificationTable(data) {
+            $('table.members').DataTable({
+                data: data,
+                'bInfo': false,
+                'order': [1, 'asc'],
+                paging: false,
+                columns: [
+                    {data: 'team',       title: 'Time' },
+                    {data: 'name',       title: 'Nome' },
+                    {data: 'first_year', title: 'Cartoleiro Desde' },
+                    {data: 'pro',        title: 'Pró' }
+                ]
+            });
+        }
 
 		getMembers()
 			.then(function(res) {
-				setMembersTable(res);
+				handleMembersData(res);
 			})
 			.catch(function(err) {
 				console.log('Something went wrong.');
