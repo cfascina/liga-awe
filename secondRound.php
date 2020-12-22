cb96d6127fac8a8bc6684483d149e332590d0e78
Caio <cfascina@gmail.com>
2020-12-22 11:35:58 -0300

    Secound round page

---
+++ secondRound.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liga AWE</title>
+	<link rel="stylesheet" href="./assets/css/styles.css">
+	<link rel="stylesheet" href="./assets/css/datatables.css">
+</head>
+
+<body>
+	<?php require_once('partials/header.html'); ?>
+
+	<div class="content second-round">
+        <h1>Aguardando nosso amigo abaixo informar os times participantes.</h1>
+        <img src="assets/images/jachetta.jpg" />
+	</div>
+
+	<?php require_once('partials/footer.html'); ?>
+
+	<script src="./assets/js/libs/jquery.min.js"></script>
+	<script src="./assets/js/menuToggle.js"></script>
+</body>
+
+</html>
